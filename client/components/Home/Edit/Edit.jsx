4d55e43e8c32b2e1a572112b62c5a9bd4e02624c
Georgi Georgiev <joro_fr@abv.bg>
2023-12-07 12:42:57 +0200

    add more functionality to edit page

--- client/components/Home/Edit/Edit.jsx
+++ client/components/Home/Edit/Edit.jsx
@@ -1,5 +1,5 @@
-import React, { useState,useContext,useEffect } from 'react';
-import { useParams } from "react-router-dom";
+import React, { useState, useContext, useEffect } from 'react';
+import { Link, useParams } from "react-router-dom";
 import UserContext from '../../../contexts/uresContext';
 import * as eventService from '../../../servises/eventService';
 import { useNavigate } from "react-router-dom";
@@ -9,80 +9,91 @@ import style from '../Edit/Edit.module.css'
 export default function Edit() {
     const { idEvent } = useParams();
     const { accessToken } = useContext(UserContext);
-  
+
     const [oneEvent, setEvent] = useState({
-      title: '',
-      summary: '',
-      imageURL: ''
+        title: '',
+        summary: '',
+        imageURL: ''
     });
-  
+
     const [title, setTitle] = useState('');
     const [summary, setSummary] = useState('');
     const [imageURL, setImageURL] = useState('');
-  
+
     const navigate = useNavigate();
-  
+
     useEffect(() => {
-      eventService
-        .getOne(idEvent)
-        .then((result) => setEvent(result))
-        .catch((err) => console.log(err));
+        eventService
+            .getOne(idEvent)
+            .then((result) => setEvent(result))
+            .catch((err) => console.log(err));
     }, [idEvent]);
-  
+
     useEffect(() => {
-      setTitle(oneEvent.title || '');
-      setSummary(oneEvent.summary || '');
-      setImageURL(oneEvent.imageURL || '');
+        setTitle(oneEvent.title || '');
+        setSummary(oneEvent.summary || '');
+        setImageURL(oneEvent.imageURL || '');
     }, [oneEvent]);
-  
+
     const handleTitleChange = (event) => {
-      setTitle(event.target.value);
+        setTitle(event.target.value);
     };
-  
+
     const handleSummaryChange = (event) => {
-      setSummary(event.target.value);
+        setSummary(event.target.value);
     };
-  
+
     const handleImageURLChange = (event) => {
-      setImageURL(event.target.value);
+        setImageURL(event.target.value);
     };
-  
+
     const handleSubmit = (event) => {
-      event.preventDefault();
-  
-      eventService.edit(idEvent, accessToken, { title, summary, imageURL });
-      navigate("/catalog");
+        event.preventDefault();
+        alert("Do you want to save the changes?")
+        eventService.edit(idEvent, accessToken, { title, summary, imageURL });
+        navigate("/catalog");
     };
-  
+
+    const cancelEditingEvent = () => {
+        navigate("/catalog")
+    }
+
     return (
-      <>
-        <div className={style.background}></div>
-        <div className={style.container}>
-          <h1 className={style.mainHeader}>Edit Event</h1>
-          <form className={style.formData} onSubmit={handleSubmit}>
-            <div>
-              <label className={style.labelForm}>Title:</label>
-              <input className={style.inputArea} type="text" value={title} onChange={handleTitleChange} />
+        <>
+            <div className={style.background}></div>
+            <div className={style.container}>
+                <h1 className={style.mainHeader}>Edit Event</h1>
+                <form className={style.formData} onSubmit={handleSubmit}>
+                    <div>
+                        <label className={style.labelForm}>Edit Title:</label>
+                        <input className={style.inputArea} type="text" value={title} onChange={handleTitleChange} />
+                    </div>
+                    <div>
+                        <label className={style.labelForm}>Edit Summary:</label>
+                        <textarea className={style.text} value={summary} onChange={handleSummaryChange} />
+                    </div>
+                    <div>
+                        <label className={style.labelForm}>Edit Image URL:</label>
+                        <input className={style.inputArea} type="text" value={imageURL} onChange={handleImageURLChange} />
+                    </div>
+                    <div className={style.actionBtn}>
+                        <div>
+                            <button type="submit">Edit</button>
+                        </div>
+                        <div>
+                            <button type="submit" onClick={cancelEditingEvent}>Canlce</button>
+                        </div>
+                    </div>
+
+
+                </form>
+                <div className={style.previewContainer}>
+                    <h2 className={style.preview}>Preview:</h2>
+                    <h3 className={style.title}>{title}</h3>
+                    <img className={style.image} src={imageURL} alt={title} />
+                    <p className={style.paragraph}>{summary}</p>
+                </div>
             </div>
-            <div>
-              <label className={style.labelForm}>Summary:</label>
-              <textarea className={style.text} value={summary} onChange={handleSummaryChange} />
-            </div>
-            <div>
-              <label className={style.labelForm}>Image URL:</label>
-              <input className={style.inputArea} type="text" value={imageURL} onChange={handleImageURLChange} />
-            </div>
-            <div>
-              <button type="submit">Edit</button>
-            </div>
-          </form>
-          <div className={style.previewContainer}>
-            <h2 className={style.preview}>Preview:</h2>
-            <h3 className={style.title}>{title}</h3>
-            <img className={style.image} src={imageURL} alt={title} />
-            <p className={style.paragraph}>{summary}</p>
-          </div>
-        </div>
-      </>
+        </>
     );
-  }
+}
